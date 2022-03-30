6a0e8dc3e809324425b7c844606013fcf3ef2632
Pavel Fokin <fokinpavel2007@outlook.com>
2022-03-30 10:52:33 +0300

    second

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,11 @@
 // TODO: write code here
 
 // comment this to pass build
-const unusedVariable = 'variable';
 
 // for demonstration purpose only
 export default function demo(value) {
   return value;
 }
 
+// eslint-disable-next-line no-console
 console.log('app.js included');
