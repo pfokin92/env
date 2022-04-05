855cff69bb4f2b98c2a0ae090b30b618582d4127
Pavel Fokin <fokinpavel2007@outlook.com>
2022-04-05 09:04:54 +0300

    test2

--- webpack.common.js
+++ webpack.common.js
@@ -6,7 +6,7 @@ module.exports = {
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    // publicPath: '/',
+    publicPath: '/',
     assetModuleFilename: 'images/[name][contenthash][ext]'
   },
   module: {
